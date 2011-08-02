e5098663c2d2ffd9526d0b8d52b6c4e15a7fe300
roofis0 <roofis20002003@yahoo.com>
2011-08-02 04:07:07 -0400

    correcting package

--- sorce/unoBotMain.java
+++ sorce/unoBotMain.java
@@ -1,13 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package uno2;
 
-/**
- *
- * @author Michael
- */
 public class unoBotMain {
 
     public static void main(String[] args)
