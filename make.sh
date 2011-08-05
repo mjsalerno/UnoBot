29f6cadc10dcbfc603ccff3eb4cd8909df7a38ea
roofis0 <roofis20002003@yahoo.com>
2011-08-04 22:14:19 -0400

    add make shell script

---
+++ make.sh
@@ -0,0 +1,18 @@
+mkdir src/lib/
+cp pircbot-1.5.0/pircbot.jar src/lib/
+cd src
+javac -cp lib/pircbot.jar uno2/*.java
+echo Class-Path: lib/pircbot.jar > man.txt
+echo Main-Class: uno2.unoBotMain >> man.txt
+jar cfm UnoBot.jar man.txt uno2 lib
+mv man.txt
+rm -r lib
+cd uno2
+rm *.class
+cd ..\..
+cp src\UnoBot.jar .
+rm src\UnoBot.jar
+mkdir lib
+cp pircbot-1.5.0\pircbot.jar lib\
+pause
+
