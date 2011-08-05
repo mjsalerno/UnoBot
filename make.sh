5bed804a708dc1f50656c581d805f9c54b11b32d
roofis0 <roofis20002003@yahoo.com>
2011-08-04 22:27:34 -0400

    fix shell script

--- make.sh
+++ make.sh
@@ -5,14 +5,14 @@ javac -cp lib/pircbot.jar uno2/*.java
 echo Class-Path: lib/pircbot.jar > man.txt
 echo Main-Class: uno2.unoBotMain >> man.txt
 jar cfm UnoBot.jar man.txt uno2 lib
-mv man.txt
+rm man.txt
 rm -r lib
 cd uno2
 rm *.class
-cd ..\..
-cp src\UnoBot.jar .
-rm src\UnoBot.jar
+cd ../..
+cp src/UnoBot.jar .
+rm src/UnoBot.jar
 mkdir lib
-cp pircbot-1.5.0\pircbot.jar lib\
+cp pircbot-1.5.0/pircbot.jar lib/
 pause
 
