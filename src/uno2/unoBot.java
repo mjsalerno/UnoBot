1b6d73b83cc938d419bc190a7cb188faa5f79709
roofis0 <roofis20002003@yahoo.com>
2011-08-12 20:41:24 -0400

    Revert "Revert "idk""
    This reverts commit 12823eb616a2ef21aacef308258fa39db9132e45.

--- src/uno2/unoBot.java
+++ src/uno2/unoBot.java
@@ -22,7 +22,7 @@ public class unoBot extends PircBot {
     private boolean gameUp = false;
     private boolean delt = false;
     private boolean drew = false;
-    private boolean cheating = true;
+    private boolean cheating = false;
     private boolean botAI = false;
     
     private Deck deck = new Deck();
@@ -48,6 +48,12 @@ public class unoBot extends PircBot {
     
     private void printPlayers(String channel){
         sendMessage(channel, players.toString());
+    }
+    
+    private void printBotOps(String channel){
+        for(String str : botOps){
+            sendMessage(channel, str);
+        }
     }
     
     private boolean isBotOp(String nick) {
@@ -116,22 +122,21 @@ public class unoBot extends PircBot {
     }
     
     private void printScore(String channel) throws FileNotFoundException{
-        //ScoreBoard.sortScore();
-        Scanner is = new Scanner(new FileInputStream("score.txt"));
-        String line = null;
-        String[] split;
-        int i = 1;
-        while(is.hasNextLine()){
-            line = is.nextLine();
-            split = line.split(" ");
-            Double a = Double.parseDouble(split[1]);
-            Double b = Double.parseDouble(split[2]);
-            Double c = a/(a+b);
-            c *= 100;
-            sendMessage(channel,"#" + i++ + " " + split[0] + ": " + split[1] + "/" + (b.intValue()+a.intValue()) + " " + c.intValue() + "%");
-                    
+        try (Scanner is = new Scanner(new FileInputStream("score.txt"))) {
+            String line = null;
+            String[] split;
+            int i = 1;
+            while(is.hasNextLine()){
+                line = is.nextLine();
+                split = line.split(" ");
+                Double a = Double.parseDouble(split[1]);
+                Double b = Double.parseDouble(split[2]);
+                Double c = a/(a+b);
+                c *= 100;
+                sendMessage(channel,"#" + i++ + " " + split[0] + ": " + split[1] + "/" + (b.intValue()+a.intValue()) + " " + c.intValue() + "%");
+                        
+            }
         }
-        is.close();
     }
     
     @Override
@@ -209,6 +214,10 @@ public class unoBot extends PircBot {
         else if ( Tokens[0].equalsIgnoreCase("!messages")){
             sendMessage(channel,msg.forUserToString());
         }
+        //GETBOTOPS
+        else if ( Tokens[0].equalsIgnoreCase("!getBotops")){
+            printBotOps(channel);
+        }
         //ENDGAME
         else if ( Tokens[0].equalsIgnoreCase("!endgame") && (isBotOp(sender) || sender.equals(gameStarter)) ) {
             gameUp = false;
@@ -246,9 +255,7 @@ public class unoBot extends PircBot {
                 bot2.setBotOps(botOps);
                 try {
                     bot2.connect(this.getServer(), this.getPort());
-                } catch (IOException ex) {
-                    Logger.getLogger(unoBot.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IrcException ex) {
+                } catch (        IOException | IrcException ex) {
                     Logger.getLogger(unoBot.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 bot2.joinChannel(channel);
