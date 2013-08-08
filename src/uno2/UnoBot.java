c7a3f8b26cd6878275f12e0dcb931be1c392d1a2
Torben Nielsen <torben@t-hoerup.dk>
2013-08-08 17:40:54 +0200

    Fix bug in D2 handling

--- src/uno2/UnoBot.java
+++ src/uno2/UnoBot.java
@@ -504,11 +504,11 @@ public class UnoBot extends ListenerAdapter<PircBotX> {
                     //D2
                     else if(card.face.equals(Card.Face.D2)){
                         player.play(card, deck);
-                        int cardCount = players.at().draw(deck, 2);
+                        int cardCount = players.next().draw(deck, 2);
                         if (cardCount == 2) {
                         	bot.sendMessage(channel, players.next().getName() + " draws 2 cards.");
                         } else {
-                        	bot.sendMessage(channel, "Deck is empty, " + players.at().getName() + " draws " + cardCount + " cards.");
+                        	bot.sendMessage(channel, "Deck is empty, " + players.next().getName() + " draws " + cardCount + " cards.");
                         }
                         
                         players.next();
