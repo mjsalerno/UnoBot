a341f6fb8eda4109d7953af3740a2566b368fc7c
Torben Hoerup Nielsen <torben@t-hoerup.dk>
2013-08-15 16:47:19 +0200

    Remove extra spaces

--- src/uno2/UnoBot.java
+++ src/uno2/UnoBot.java
@@ -209,7 +209,7 @@ public class UnoBot extends ListenerAdapter<PircBotX> {
     @Override
 	public void onMessage(MessageEvent<PircBotX> event) throws Exception {
     	String message = event.getMessage().trim();
-    	message = message.replaceAll("  ", " ");//remove double spaces
+    	message = message.replaceAll("\\s{2,99}", " ");//remove extra spaces
     	
         String[] tokens = message.split(" ");
         String sender = event.getUser().getNick();
