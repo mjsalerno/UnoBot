d08b2b498b7499cac5e900ef1075b958bf91d513
Scott <scott@scotts.cc>
2021-04-05 20:37:53 +0100

    re fix :/

--- src/main/java/com/mjsalerno/unobot/UnoBot.java
+++ src/main/java/com/mjsalerno/unobot/UnoBot.java
@@ -828,13 +828,9 @@ public class UnoBot extends ListenerAdapter {
 		
 		deck.createDeck(this.extreme);
 		
-<<<<<<< HEAD
+
 		if (autoAI && players.size() == 1 && botAI == false) {
 			bot.sendIRC().message(gameChannel, "There is only one player in this game, launching AI player");
-=======
-		if (autoAI && players.size() == 1) {
-			bot.sendIRC().message(gameChannel, "Only one player in this game, launching AI player");
->>>>>>> parent of 7c9a626 (change some spelling errors to put my pedant mind to rest and shorten commands)
 			startAIthread();
 		}
 		
